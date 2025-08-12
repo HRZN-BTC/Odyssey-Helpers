42e3299df241cc2b46a0017aedf41e812aaf22d3
oogunjob <togunjobi13@yahoo.com>
2025-08-11 21:15:18 -0700

    updated daily bitthought fetcher

--- send_update_daily_bitthought.py
+++ send_update_daily_bitthought.py
@@ -3,6 +3,9 @@ from supabase import create_client, Client
 import onesignal
 from onesignal.api import default_api
 from onesignal.model.notification import Notification
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Supabase credentials
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
@@ -10,14 +13,20 @@ SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
 # One Signal credentials
 ONE_SIGNAL_APP_KEY = os.environ.get("ONE_SIGNAL_APP_KEY")
-ONE_SIGNAL_USER_KEY = os.environ.get("ONE_SIGNAL_USER_KEY")
-ONE_SIGNAL_APP_ID = os.environ.get("ONE_SIGNAL_USER_KEY")
+ONE_SIGNAL_APP_ID = os.environ.get("ONE_SIGNAL_APP_ID")
 
 # OneSignal configuration
 configuration = onesignal.Configuration(
     app_key=ONE_SIGNAL_APP_KEY,
-    user_key=ONE_SIGNAL_USER_KEY,
 )
+
+def fetch_daily_bitthought():
+    """
+    Fetches daily bitthoughts from the database.
+    This function is a placeholder and should be implemented as needed.
+    """
+    pass
+    
 
 def send_update_daily_bitthought():
     """
@@ -42,22 +51,21 @@ def send_update_daily_bitthought():
         with onesignal.ApiClient(configuration) as api_client:
             api_instance = default_api.DefaultApi(api_client)
             
-            # Create notification
-            notification = Notification(
-                app_id=ONE_SIGNAL_APP_ID,
-                headings={"en": "Captain's Log"},
-                contents={"en": "Ahoy matey, ship sailing"},
-                include_aliases={"external_id": user_ids},
-                target_channel="push",
-                ios_badge_type="Increase",
-                ios_badge_count=1,
-            )
-            
-            # Send notification
-            api_response = api_instance.create_notification(notification)
-            print(f"Notification sent successfully! Response: {api_response}")
-            
-            return len(user_ids)
+        # Create notification
+        notification = Notification(
+            app_id=ONE_SIGNAL_APP_ID,
+            headings={"en": "Captain's Log"},
+            contents={"en": "Ahoy matey, ship sailing"},
+            include_aliases={"external_id": user_ids},
+            target_channel="push",
+            ios_badge_type="Increase",
+            ios_badge_count=1,
+        )
+        
+        # Send notification
+        _ = api_instance.create_notification(notification)
+        
+        return len(user_ids)
             
     except Exception as e:
         print(f"Error sending daily captain's log notification: {str(e)}")
